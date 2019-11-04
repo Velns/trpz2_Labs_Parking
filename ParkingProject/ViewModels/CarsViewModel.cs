063e0594088fd802b398491327d387fb4f5091c8
Laiks <den.denis999@gmail.com>
2019-11-04 21:33:21 +0200

    test

--- ParkingProject/ViewModels/CarsViewModel.cs
+++ ParkingProject/ViewModels/CarsViewModel.cs
@@ -26,10 +26,6 @@ using System.Collections.Generic;
         public ObservableCollection<User> UserList { get; set; }
 
         public override string Title { get { return "View model title"; } }
-
-        // TODO: Register models with the vmpropmodel codesnippet
-        // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
-        // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
         
         protected override async Task InitializeAsync()
         {
